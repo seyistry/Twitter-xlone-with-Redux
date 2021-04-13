2cf9750064b1ab619d09573f9d03e06f7296c1ef
seyi egunjobi <seyistry@gmail.com>
2021-04-13 15:08:13 +0100

    add main Dashboard component

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,25 @@ import * as React from "react";
 import { Component } from "react";
 import { connect } from "react-redux";
 import { handleIntialData } from "../actions/shared";
+import Dashboard from "./Dashboard";
 
 class App extends Component {
     componentDidMount() {
-        this.props.dispatch(handleIntialData())
+        this.props.dispatch(handleIntialData());
     }
     render() {
-        return <div className="container">Twiiter Clone</div>;
+        return (
+            <div className="container">
+                {this.props.loading === true ? null : <Dashboard />}
+            </div>
+        );
     }
 }
-export default connect()(App);
+
+function mapStateToProps({ authedUser }) {
+    return {
+        loading: authedUser === null,
+    };
+}
+
+export default connect(mapStateToProps)(App);
